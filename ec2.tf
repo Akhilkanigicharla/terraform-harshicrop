1014538ee8df88e5fed16da1c0cd4d76b2a0e931
Akhilkanigicharla <kanigicharlaakhil9@gmail.com>
2021-07-25 16:03:59 +0530

    new

---
+++ ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "sample" {
+  ami                   = "ami-059e6ca6474628ef0"
+  instance_type         = "t3.micro"
+  user_data             = file("${path.module}/app1-install.sh")
+  tags = {
+    "Name" = "EC3 Demo"
+  }
+}
